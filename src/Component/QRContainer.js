5643c4479bc8ffb531e79be3b67b5e23b08a8b5b
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-13 20:31:02 +0530

    Update QRContainer.js

--- src/Component/QRContainer.js
+++ src/Component/QRContainer.js
@@ -190,7 +190,9 @@ class QRContainer extends React.Component {
         //     let url = "https://kwqr.me/98138FL41618/J";
         // let url = "https://kwqr.me/85303D62177"
         // let url = "https://kwqr.me/85303D62177/D"
-        let url = "https://kwqr.me/98138FL41618";
+//         let url = "https://kwqr.me/98138FL41618";
+        // let url = "https://kwqr.me/85303D62177/D"
+        let url = "https://kwqr.me/98138F41618";
         let query = url.substring(16);
         console.log("query: ", query);
         let res;
