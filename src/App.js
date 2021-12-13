a62cb290f1acffb8876310a80b146d7fec7cbd64
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-13 15:57:17 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import QRContainer from "./Component/QRContainer";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          {/* <QRContainer /> */}
-          <Route path="/" element={<QRContainer />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <QRContainer />
+    </div>
   );
 }
 
