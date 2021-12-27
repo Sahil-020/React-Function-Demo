16e7b7dfc838cf3db8f695abec66a4b2b16b1642
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-27 14:08:59 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,19 +10,21 @@ function App() {
   const [showLoader, setShowLoader] = useState(false);
   return (
     // <LoadingOverlay active={showLoader} spinner text="Loading...">
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          {/* <QRContainer /> */}
-          <Route
-            path="/"
-            component={() => <QRContainer setShowLoader={setShowLoader} />}
-          />
-          {/* <Route exact path="/:id" component={QRContainer} /> */}
-        </Switch>
-        <ToastContainer hideProgressBar={true} />
-      </div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            {/* <QRContainer /> */}
+            <Route
+              path="/"
+              component={() => <QRContainer setShowLoader={setShowLoader} />}
+            />
+            {/* <Route exact path="/:id" component={QRContainer} /> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
+      <ToastContainer hideProgressBar={true} />
+    </>
     // </LoadingOverlay>
   );
 }
